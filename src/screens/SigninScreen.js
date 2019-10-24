7485ae50ce8a7468269491145dd691ee4d118fda
Katie Rose <katiefelten@gmail.com>
2019-10-24 16:52:55 -0400

    :zap: created a boilerplate for screens

---
+++ src/screens/SigninScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const SignInScreen = () => {
+  return <Text style={{ fontSize: 48 }}>Sign In Screen</Text>;
+};
+
+const styles = Stylesheet.create({});
+
+export default SignInScreen;
---
+++ src/screens/SingupScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const SignUpScreen = () => {
+  return <Text style={{ fontSize: 48 }}>Sign Up Screen</Text>;
+};
+
+const styles = Stylesheet.create({});
+
+export default SignUpScreen;
