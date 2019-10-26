56336b1065b7d1a0d2a59b3703e4880ff0954e01
Katie Rose <katiefelten@gmail.com>
2019-10-25 20:57:37 -0400

    :zap: updated auth context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,7 +1,7 @@
+import { AsyncStorage } from "react-native";
 import createDataContext from "./createDataContext";
 import trackerApi from "../api/tracker";
 import { navigate } from "../navigationRef";
-import { AsyncStorage } from "react-native";
 
 const authReducer = (state, action) => {
   switch (action.type) {
@@ -18,16 +18,57 @@ const authReducer = (state, action) => {
   }
 };
 
+const tryLocalSignin = dispatch => async () => {
+  const token = await AsyncStorage.getItem("token");
+  if (token) {
+    dispatch({ type: "signin", payload: token });
+    navigate("TrackList");
+  } else {
+    navigate("Signup");
+  }
+};
+
+const clearErrorMessage = dispatch => () => {
+  dispatch({ type: "clear_error_message" });
+};
+
 const signup = dispatch => async ({ email, password }) => {
-  // signup
+  try {
+    const response = await trackerApi.post("/signup", { email, password });
+    await AsyncStorage.setItem("token", response.data.token);
+    dispatch({ type: "signin", payload: response.data.token });
+
+    navigate("TrackList");
+  } catch (err) {
+    dispatch({
+      type: "add_error",
+      payload: "Something went wrong with sign up"
+    });
+  }
 };
 
 const signin = dispatch => async ({ email, password }) => {
-  // signin
+  try {
+    const response = await trackerApi.post("/signin", { email, password });
+    await AsyncStorage.setItem("token", response.data.token);
+    dispatch({ type: "signin", payload: response.data.token });
+    navigate("TrackList");
+  } catch (err) {
+    dispatch({
+      type: "add_error",
+      payload: "Something went wrong with sign in"
+    });
+  }
+};
+
+const signout = dispatch => async () => {
+  await AsyncStorage.removeItem("token");
+  dispatch({ type: "signout" });
+  navigate("loginFlow");
 };
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
-  { isSignedIn: false }
+  { signin, signout, signup, clearErrorMessage, tryLocalSignin },
+  { token: null, errorMessage: "" }
 );
