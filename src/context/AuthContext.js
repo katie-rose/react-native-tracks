12e4b8dff22aede51ec1f0b10ec20e3b77b9bc3c
Katie Rose <katiefelten@gmail.com>
2019-10-25 20:53:50 -0400

    :zap: added authform, map, navlink, spacer, trackform

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,7 +1,18 @@
 import createDataContext from "./createDataContext";
+import trackerApi from "../api/tracker";
+import { navigate } from "../navigationRef";
+import { AsyncStorage } from "react-native";
 
 const authReducer = (state, action) => {
   switch (action.type) {
+    case "add_error":
+      return { ...state, errorMessage: action.payload };
+    case "signin":
+      return { errorMessage: "", token: action.payload };
+    case "clear_error_message":
+      return { ...state, errorMessage: "" };
+    case "signout":
+      return { token: null, errorMessage: "" };
     default:
       return state;
   }
