9875db8b8a0ab246dd079262dc6fb9c506348f3b
Katie Rose <katiefelten@gmail.com>
2019-10-25 20:56:41 -0400

    :zap: updated location and track context

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,8 +1,8 @@
 import React, { useContext } from "react";
 import { Input, Button } from "react-native-elements";
 import Spacer from "./Spacer";
-// import { Context as LocationContext } from "../context/LocationContext";
-// import useSaveTrack from "../hooks/useSaveTrack";
+import { Context as LocationContext } from "../context/LocationContext";
+import useSaveTrack from "../hooks/useSaveTrack";
 
 const TrackForm = () => {
   const {
